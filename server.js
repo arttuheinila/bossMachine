3d91d18c4c44829cd827f97c9f694b43a03efbb8
Arttu Heinilä <63282225+arttuheinila@users.noreply.github.com>
2023-10-19 10:12:24 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,9 +3,7 @@ const app = express();
 
 module.exports = app;
 app.use(express.static(__dirname))
-/* Do not change the following line! It is required for testing and allowing
-*  the frontend application to interact as planned with the api server
-*/
+
 const PORT = process.env.PORT || 4001;
 
 // Add middleware for handling CORS requests from index.html
@@ -20,9 +18,8 @@ app.use(bodyParser.json())
 const apiRouter = require('./server/api');
 app.use('/api', apiRouter)
 
-// This conditional is here for testing purposes:
+// Conditional for testing purposes:
 if (!module.parent) { 
-  // Add your code to start the server listening at PORT below:
   app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
   });
